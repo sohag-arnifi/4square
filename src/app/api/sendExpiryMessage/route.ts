4901400e383f9f01a4d3186c4630d0ac0d2f13ca
sohag-arnifi <sohag.b@arnifi.com>
2024-07-28 21:46:37 +0600

    corn job attem75

--- src/app/api/sendExpiryMessage/route.ts
+++ src/app/api/sendExpiryMessage/route.ts
@@ -1,6 +1,25 @@
+import { IBulkSMS } from "@/models/bulksms";
+import bulksmsControllers from "@/server/controllers/bulksms";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  console.log("corn jobs working successfully");
-  return NextResponse.json({ ok: true });
+  const data = {
+    sendTo: null,
+    sendBy: "corn_job",
+    sendNumber: "01760567555",
+    message: `corn job run at ${new Date().toLocaleString()}`,
+    smsCount: 1,
+  };
+  try {
+    const response = await bulksmsControllers.sendSingle(data as IBulkSMS);
+    console.log(
+      `corn jobs working successfully, at: ${new Date().toLocaleString()}`
+    );
+
+    console.log(response);
+
+    return NextResponse.json({ ok: true });
+  } catch (error) {
+    console.log(error, "Error");
+  }
 }
